c508ece5b793dd5c67853740d0036faed29b2945
JackY <wo131485508@qq.com>
2016-05-11 17:09:22 -0400

    CellXib

---
+++ CalorieCount/FoodCell.swift
@@ -0,0 +1,19 @@
+//
+//  FoodCell.swift
+//  CalorieCount
+//
+//  Created by Yaxin Yuan on 16/5/11.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FoodCell: UITableViewCell{
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var calorieLabel: UILabel!
+    
+    
+    
+    
+}
--- CalorieCount/ViewController.swift
+++ CalorieCount/ViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private struct commonConstants{
+        static let rowHeight:CGFloat = 80
+        static let topInsets:CGFloat = 64
+        static let cellXib = "FoodCell"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: commonConstants.topInsets, left: 0, bottom: 0, right: 0)
+        let cellNib = UINib(nibName: commonConstants.cellXib, bundle: nil)
+        tableView.registerNib(cellNib, forCellReuseIdentifier: commonConstants.cellXib)
+        tableView.rowHeight = commonConstants.rowHeight
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(commonConstants.cellXib, forIndexPath: indexPath) as! FoodCell
+        cell.foodLabel.text = "jack"
+        cell.calorieLabel.text = "haha"
+        return cell
+    }
+    
+    
 
 
 }
