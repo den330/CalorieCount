6df71760d9e99f8da0c5fa5bf629aa53ee8a8eeb
JackY <wo131485508@qq.com>
2016-05-15 12:33:30 -0400

    totalCalories in a day

--- CalorieCount/RecordTableViewController.swift
+++ CalorieCount/RecordTableViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import CoreData
 
+
 class RecordTableViewController: UITableViewController {
     
     var fetchedResultsController: NSFetchedResultsController!
     let fetchRequest = NSFetchRequest(entityName: "Day")
     var managedContext: NSManagedObjectContext!
+    let dateFormatter = NSDateFormatter()
     
    
     
@@ -21,6 +23,7 @@ class RecordTableViewController: UITableViewController {
     override func viewDidLoad() {
         print("haha")
         super.viewDidLoad()
+        dateFormatter.dateStyle = .MediumStyle
         let sortDescriptor = NSSortDescriptor(key: "currentDate", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
         fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
@@ -39,8 +42,19 @@ class RecordTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("recordCell", forIndexPath: indexPath)
         let day = fetchedResultsController.objectAtIndexPath(indexPath) as! Day
+        let items = day.items!
+        var totalCalories = 0.0
+        for i in items{
+            let item = i as! ItemConsumed
+            totalCalories += Double(item.totalCalories!)
+        }
+        
+        
         let dateLabel =  cell.viewWithTag(1000) as! UILabel
-        dateLabel.text = String(day.currentDate!)
+        let caloriesLabel = cell.viewWithTag(1001) as! UILabel
+        let today = dateFormatter.stringFromDate(day.currentDate!)
+        dateLabel.text = today
+        caloriesLabel.text = "Total Calories Consumed: " + String(totalCalories) + " kCal"
         return cell
     }
     
