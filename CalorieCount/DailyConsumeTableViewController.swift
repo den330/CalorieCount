fa443562e6116d4f9c3a38bcc29dac7488654db2
JackY <wo131485508@qq.com>
2016-05-15 13:14:48 -0400

    finished without delete and edit

---
+++ CalorieCount/DailyConsumeTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DailyConsumeTableViewController.swift
+//  CalorieCount
+//
+//  Created by Yaxin Yuan on 16/5/15.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DailyConsumeTableViewController: UITableViewController{
+    var day: Day!
+    var items: NSOrderedSet?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        items = day.items
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items!.count
+    }
+    
+    @IBAction func back(){
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("item", forIndexPath: indexPath)
+        let contentLabel = cell.viewWithTag(1000) as! UILabel
+        let caloriesLabel = cell.viewWithTag(1002) as! UILabel
+        let quantityLabel = cell.viewWithTag(1001) as! UILabel
+        let item = day.items![indexPath.row] as! ItemConsumed
+        contentLabel.text = item.name
+        caloriesLabel.text = "Total Calories: " + String(item.totalCalories!)
+        quantityLabel.text = "Quantity Consumed: " + String(item.quantityConsumed!)
+        return cell
+    }
+}
--- CalorieCount/RecordTableViewController.swift
+++ CalorieCount/RecordTableViewController.swift
@@ -17,11 +17,7 @@ class RecordTableViewController: UITableViewController {
     var managedContext: NSManagedObjectContext!
     let dateFormatter = NSDateFormatter()
     
-   
-    
-
     override func viewDidLoad() {
-        print("haha")
         super.viewDidLoad()
         dateFormatter.dateStyle = .MediumStyle
         let sortDescriptor = NSSortDescriptor(key: "currentDate", ascending: true)
@@ -42,20 +38,35 @@ class RecordTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("recordCell", forIndexPath: indexPath)
         let day = fetchedResultsController.objectAtIndexPath(indexPath) as! Day
+        configureCell(cell, day: day)
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell, day: Day){
         let items = day.items!
         var totalCalories = 0.0
         for i in items{
             let item = i as! ItemConsumed
             totalCalories += Double(item.totalCalories!)
         }
-        
-        
         let dateLabel =  cell.viewWithTag(1000) as! UILabel
         let caloriesLabel = cell.viewWithTag(1001) as! UILabel
         let today = dateFormatter.stringFromDate(day.currentDate!)
         dateLabel.text = today
         caloriesLabel.text = "Total Calories Consumed: " + String(totalCalories) + " kCal"
-        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("showthatday", sender: indexPath)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showthatday"{
+            let index = sender as! NSIndexPath
+            let naviController = segue.destinationViewController as! UINavigationController
+            let dayController = naviController.topViewController as! DailyConsumeTableViewController
+            dayController.day = fetchedResultsController.objectAtIndexPath(index) as! Day
+        }
     }
     
   
