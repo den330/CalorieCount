fdf93d82307178619a5e706f3d671c4a046cf31d
JackY <wo131485508@qq.com>
2016-05-30 21:18:50 -0400

    Pop-Up Animation

--- CalorieCount/DetailAnimationController.swift
+++ CalorieCount/DetailAnimationController.swift
@@ -11,9 +11,8 @@ import UIKit
 
 class DetailAnimationController: NSObject, UIViewControllerAnimatedTransitioning{
     
-    let duration = 0.5
+    let duration = 0.3
     var presenting = true
-    var originFrame = CGRect.zero
     var count = 0
     
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
@@ -25,21 +24,36 @@ class DetailAnimationController: NSObject, UIViewControllerAnimatedTransitioning
         if presenting{
             let toView = transitionContext.viewForKey(UITransitionContextToViewKey)!
             containerView.addSubview(toView)
-            toView.alpha = 0.0
-            UIView.animateWithDuration(duration, animations: {
-                toView.alpha = 1.0
-                }, completion: {_ in
-                    transitionContext.completeTransition(true)})
+            toView.transform = CGAffineTransformMakeScale(0.4, 0.4)
+            UIView.animateKeyframesWithDuration(transitionDuration(transitionContext), delay: 0, options: .CalculationModeCubic, animations: {
+                UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.334, animations: {
+                    toView.transform = CGAffineTransformMakeScale(1.3, 1.3)
+                })
+                UIView.addKeyframeWithRelativeStartTime(0.334, relativeDuration: 0.333, animations: {
+                    toView.transform = CGAffineTransformMakeScale(0.7, 0.7)
+                })
+                UIView.addKeyframeWithRelativeStartTime(0.666, relativeDuration: 0.333, animations: {
+                    toView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+                })
+                }, completion: {
+                    finished in transitionContext.completeTransition(finished)
+            })
         }else{
+            if count == 3{
+                count = 0
+            }
             count += 1
             let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
             containerView.addSubview(fromView)
             fromView.alpha = 1.0
             UIView.animateWithDuration(duration, animations: {
-                if self.count % 2 == 0{
+                fromView.transform = CGAffineTransformMakeScale(0.5, 0.5)
+                if self.count == 1{
                     fromView.center.x += containerView.bounds.width
-                }else{
+                }else if self.count == 2{
                     fromView.center.x -= containerView.bounds.width
+                }else{
+                    fromView.alpha = 0.0
                 }}, completion: { _ in
                 transitionContext.completeTransition(true)})
         }
