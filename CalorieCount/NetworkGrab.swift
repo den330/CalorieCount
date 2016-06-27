4ffe3945457e61c2a2ec24fcc5bc9c81a7fca9be
JackY <wo131485508@qq.com>
2016-06-27 19:48:24 -0400

    Alamofire Imported

--- CalorieCount/NetworkGrab.swift
+++ CalorieCount/NetworkGrab.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SystemConfiguration
+import Alamofire
 
 
 
---
+++ Pods/Target Support Files/Pods-CalorieCount/Pods-CalorieCount-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CalorieCountVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CalorieCountVersionString[];
+
